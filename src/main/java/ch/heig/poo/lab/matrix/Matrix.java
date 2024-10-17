6776c7c720b8d073ebd5e079ff6f56e7ee5cce89
Chomusuke <yukhanov.artur@gmail.com>
2024-10-17 15:25:09 +0200

    Basics for Matrix class

---
+++ src/main/java/ch/heig/poo/lab/matrix/Matrix.java
@@ -0,0 +1,18 @@
+package ch.heig.poo.lab.matrix;
+
+public class Matrix {
+	private final int n;
+	private final int m;
+	private final int modulus;
+
+	public Matrix(int n, int m, int modulus) {
+		this.n = n;
+		this.m = m;
+		this.modulus = modulus;
+	}
+
+	@Override
+	public String toString() {
+		return "";
+	}
+}
