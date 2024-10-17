01e5ad14e36aa5e29e6e5668485a58a031c2178b
Chomusuke <yukhanov.artur@gmail.com>
2024-10-17 15:55:07 +0200

    Added check for null input matrices

--- src/main/java/ch/heig/poo/lab/matrix/operation/Operation.java
+++ src/main/java/ch/heig/poo/lab/matrix/operation/Operation.java
@@ -6,6 +6,9 @@ public abstract class Operation {
 	public Operation() {}
 
 	public Matrix apply(Matrix a, Matrix b) {
+		if (a == null || b == null) {
+			throw new NullPointerException("Matrix cannot be null");
+		}
 		if (a.getModulus() != b.getModulus()) {
 			throw new RuntimeException("Matrices have different modulos");
 		}
