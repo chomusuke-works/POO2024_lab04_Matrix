0fd895c3bde7727727aa99da7a81bdf47f88c7d3
Chomusuke <yukhanov.artur@gmail.com>
2024-10-17 15:42:25 +0200

    Implemented base apply method: returns the initial matrix

--- src/main/java/ch/heig/poo/lab/matrix/operation/Operation.java
+++ src/main/java/ch/heig/poo/lab/matrix/operation/Operation.java
@@ -1,5 +1,15 @@
 package ch.heig.poo.lab.matrix.operation;
 
-public abstract class Operation {
+import ch.heig.poo.lab.matrix.Matrix;
 
+public abstract class Operation {
+	public Operation() {}
+
+	public Matrix apply(Matrix a, Matrix b) {
+		if (a.getModulus() != b.getModulus()) {
+			throw new RuntimeException("Matrices have different modulos");
+		}
+
+		return a;
+	}
 }
