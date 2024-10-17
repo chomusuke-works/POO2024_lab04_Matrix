d77e57ac1278c5935b5acf807d94958c506baf61
Chomusuke <yukhanov.artur@gmail.com>
2024-10-17 15:41:36 +0200

    Implemented getters

--- src/main/java/ch/heig/poo/lab/matrix/Matrix.java
+++ src/main/java/ch/heig/poo/lab/matrix/Matrix.java
@@ -1,14 +1,26 @@
 package ch.heig.poo.lab.matrix;
 
 public class Matrix {
-	private final int n;
 	private final int m;
+	private final int n;
 	private final int modulus;
 
-	public Matrix(int n, int m, int modulus) {
-		this.n = n;
+	public Matrix(int m, int n, int modulus) {
 		this.m = m;
+		this.n = n;
 		this.modulus = modulus;
+	}
+
+	public int getM() {
+		return m;
+	}
+
+	public int getN() {
+		return n;
+	}
+
+	public int getModulus() {
+		return modulus;
 	}
 
 	@Override
