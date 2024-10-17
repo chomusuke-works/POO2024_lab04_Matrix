652b855ab52bf580c468bdc0a326062602c1f2a0
Chomusuke <yukhanov.artur@gmail.com>
2024-10-17 16:04:51 +0200

    Made apply abstract in Operation class, added checkMatrixPreconditions method

--- src/main/java/ch/heig/poo/lab/matrix/operation/Operation.java
+++ src/main/java/ch/heig/poo/lab/matrix/operation/Operation.java
@@ -4,14 +4,14 @@ import ch.heig.poo.lab.matrix.Matrix;
 
 public abstract class Operation {
 
-	public Matrix apply(Matrix A, Matrix B) {
+	public abstract Matrix apply(Matrix A, Matrix B);
+
+	protected static void checkMatrixPreconditions(Matrix A, Matrix B) {
 		if (A == null || B == null) {
 			throw new NullPointerException("Matrix cannot be null");
 		}
 		if (A.getModulus() != B.getModulus()) {
 			throw new RuntimeException("Matrices have different modulos");
 		}
-
-		return A.copy();
 	}
 }
