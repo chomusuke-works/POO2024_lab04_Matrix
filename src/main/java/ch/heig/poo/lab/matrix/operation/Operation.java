37a0a6c5f7b633d8fa3b56ccd45874159fb276e1
Chomusuke <yukhanov.artur@gmail.com>
2024-10-29 16:42:15 +0100

    Reordered methods in Operation, added javadoc

--- src/main/java/ch/heig/poo/lab/matrix/operation/Operation.java
+++ src/main/java/ch/heig/poo/lab/matrix/operation/Operation.java
@@ -3,6 +3,15 @@ package ch.heig.poo.lab.matrix.operation;
 import ch.heig.poo.lab.matrix.Matrix;
 
 public abstract class Operation {
+
+	/**
+	 * Applies the current operation on the two given matrices.
+	 *
+	 * @param A the first matrix
+	 * @param B the second matrix
+	 *
+	 * @return the resulting matrix
+	 */
 	public Matrix apply(Matrix A, Matrix B) {
 		checkMatrixPreconditions(A, B);
 
@@ -21,11 +30,34 @@ public abstract class Operation {
 		return result;
 	}
 
-	// https://stackoverflow.com/questions/4467539/javascript-modulo-gives-a-negative-result-for-negative-numbers
+	protected abstract int componentOperation(int a, int b);
+
+
+
+	/**
+	 * Returns the absolute modulus of a value.
+	 * Implementation found on <a href="https://stackoverflow.com/questions/4467539/javascript-modulo-gives-a-negative-result-for-negative-numbers">StackOverflow</a>
+	 *
+	 * @param value an integer value
+	 * @param mod the modulus
+	 *
+	 * @return the absolute modulus
+	 */
 	private int absMod(int value, int mod) {
 		return ((value % mod) + mod) % mod;
 	}
 
+	/**
+	 * Checks if the two given matrices correspond to the following criteria:
+	 * - none of them is null
+	 * - their modulos are the same
+	 *
+	 * @param A the first matrix
+	 * @param B the second matrix
+	 *
+	 * @throws NullPointerException if a matrix is null
+	 * @throws RuntimeException if the matrices' modulos are different
+	 */
 	private static void checkMatrixPreconditions(Matrix A, Matrix B) {
 		if (A == null || B == null) {
 			throw new NullPointerException("Matrix cannot be null");
@@ -34,6 +66,4 @@ public abstract class Operation {
 			throw new RuntimeException("Matrices have different modulos");
 		}
 	}
-
-	protected abstract int componentOperation(int a, int b);
 }
